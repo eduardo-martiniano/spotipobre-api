d41e3923f032d520be9d1881b9630da0a0dc2254
eduardo-martiniano <eeduardo.martiniano@gmail.com>
2023-03-30 22:42:28 -0300

    add back e front pra subir com docker-compose

--- src/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const spotipobreController = require('./controllers/spotipobreController');
-const router = express.Router()
-
-router.get("/", (req, res) => res.json("online"));
-router.get("/downloadmp3", spotipobreController.downloadmp3);
-router.get("/validate", spotipobreController.validate);
-module.exports = router
